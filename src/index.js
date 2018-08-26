78b6c0253ebe31c886c19a16733cbcbc0a60c4a7
jpacsai <judit.pacsai@gmail.com>
2018-08-26 18:41:43 +0200

    refactor: switch to hashrouter from browserrouter

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
+import createBrowserHistory from 'history/createBrowserHistory'
+
+const history = createBrowserHistory()
 
 ReactDOM.render(
-    <BrowserRouter>
+    <HashRouter history={history}>
         <App />
-    </BrowserRouter>
+    </HashRouter>
     ,document.getElementById('root'))
